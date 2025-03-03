5f63a35f56b04931715773327e37633a4b387573
Ahmad-32-3 <muhammadahmad436654@gmail.com>
2025-03-03 16:57:14 -0500

    trying the workflow again

--- index.js
+++ index.js
@@ -5,7 +5,6 @@ const fetch = require('node-fetch');
 const octokit = new Octokit({ auth: process.env.GITHUB_TOKEN });
 
 // Testing Pull Request
-console.log("Hello World");
 
 const REPO_OWNER = 'Ahmad15523';  
 const REPO_NAME = 'github-actions-bot';
